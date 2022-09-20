671f06947783facd131e0c5adde1bed8ef7ce872
GiedreSavickiene <78999134+GiedreSavickiene@users.noreply.github.com>
2022-09-20 19:58:23 +0300

    Section Values

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import MainHeader from '../../components/MainHeader'
 import Programs from '../../components/Programs'
+import Values from '../../components/Values'
 import './home.css'
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <>
     <MainHeader/>
     <Programs/>
+    <Values/>
     </>
     
     
