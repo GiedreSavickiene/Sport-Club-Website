0ab03182efcc560062556d18fd755d0e7634f719
GiedreSavickiene <78999134+GiedreSavickiene@users.noreply.github.com>
2022-09-20 19:05:38 +0300

    Section Programs

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import MainHeader from '../../components/MainHeader'
+import Programs from '../../components/Programs'
 import './home.css'
 
 const Home = () => {
   return (
     <>
     <MainHeader/>
-    
+    <Programs/>
     </>
+    
+    
   )
 }
 
